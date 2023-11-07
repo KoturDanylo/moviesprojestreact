9ba45c2edb9739f4193985453659fa87f8ccbf38
danylokotur <kotur.danylo@gmail.com>
2023-11-07 22:23:09 +0200

    dxd

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import css from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h3>The MovieDB</h3>
+            <div>
+            <h3>Movies</h3>
+            <h3>Genres</h3>
+            <h3>Search</h3>
+            </div>
+
+            <div>Dark Theme</div>
+        </div>
+    );
+};
+
+export {Header};
